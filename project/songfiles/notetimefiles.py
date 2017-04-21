377dfb7ef4a72a3d16df8afc45d1f7e1f6b34015
Matthew Anthony DAlonzo <mdalonz1@fitz77.helios.nd.edu>
2017-04-21 15:32:04 -0400

    messed up

--- project/songfiles/notetimefiles.py
+++ project/songfiles/notetimefiles.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python2.7
 # notetimefiles.py
 # This will run AUBIOPITCH on whatever mp3 you want
 
 import sys
 import os
 
-timesCommand = "aubiopitch ./" + sys.argv[1] + " | awk '{print $1}' > times.txt"
-notesCommand = "aubiopitch ./" + sys.argv[1] + " | awk '{print $2}' > notes.txt"
+libraryCommand="LD_LIBRARY_PATH=/usr/local/lib:./../aubio/aubio-0.4.5/build/src"
 
+timesCommand="./aubiopitch ./" + sys.argv[1] + " | awk '{print $1}' > times.txt"
+
+notesCommand="./aubiopitch ./" + sys.argv[1] + " | awk '{print $2}' > notes.txt"
+
+os.system(libraryCommand)
+os.system("export LD_LIBRARY_PATH")
 os.system(timesCommand)
 os.system(notesCommand)
 
